e01ce0a178894e9b8769c1ab8e04f43ab8a6ae62
Jason Zhao <zhaojason68@gmail.com>
2025-05-23 16:03:56 -0400

    Full-Stack Project

---
+++ server/app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+import os
+
+db = SQLAlchemy()
+jwt = JWTManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///site.db')
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'super-secret')
+
+    db.init_app(app)
+    jwt.init_app(app)
+
+    from .routes import main
+    app.register_blueprint(main.bp)
+
+    return app
